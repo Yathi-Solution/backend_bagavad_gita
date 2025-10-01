1a3945bfbb85cc858ed8bf024e574b23da5dba71
Sathanand Uppuluri <usathanand@yathisolutions.com>
2025-10-01 16:40:07 +0530

    fix: relative import error

--- app1/services/pinecone_services.py
+++ app1/services/pinecone_services.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 from pinecone import Pinecone, ServerlessSpec
 import time
 from tqdm import tqdm
-from .embeddings import embed_text
+from services.embeddings import embed_text
 
 load_dotenv()
 
